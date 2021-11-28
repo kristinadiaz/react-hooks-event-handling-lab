ee5eb85dc0062245c6d147aac1a914e16ec4bcfb
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-28 12:10:06 -0700

    commit tree f615e185c9bc8582cfd7c960385b2f2b4a195bec

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    return (
+        <button
+            onFocus={(e) => {
+                if (e.currentTarget === e.target) {
+                    console.log('Good!');
+                }
+            }}
+            onBlur={(e) => {
+                if (e.currentTarget === e.target) {
+                    console.log('Hey! Eyes on me!');
+                }
+            }}
+        >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
